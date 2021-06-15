a8de762ea5610a3337d422d51b87890505948dff
allenlin90 <ht016746@gmail.com>
2021-06-15 16:45:55 +0700

    update and fix postcode list for creating order

--- src/store/host.js
+++ src/store/host.js
@@ -3,6 +3,6 @@
 //     product: 'https://eridu-app.herokuapp.com',
 // }
 
-export default 'http://localhost:3000';
+// export default 'http://localhost:3000';
 
-// export default 'https://eridu-app.herokuapp.com';
+export default 'https://eridu-app.herokuapp.com';
